a566c95ff1b8f977c5a426d46c4de1f09848fdd5
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-04-07 14:11:27 +0000

    Ajout fonction de calcul de coords du vecteur et correction typographique

--- commands/mathAPI.js
+++ commands/mathAPI.js
@@ -1,11 +1,24 @@
-/** @file mathAPI */
+/** @file mathAPI.js */
 
 /**
- Retourne la norme du vecteur fourni en entree
+ Retourne la norme du vecteur fourni en entrée
 * @param {number} vecX Cordonnees x du vecteur
 * @param {number} vecY Cordonnees y du vecteur
 * @returns {number} Norme obtenue
 */
 function getVectorNorme(vecX,vecY){
     return Math.sqrt(Math.pow(vecX,2)+Math.pow(vecY,2));
+}
+
+/**
+* Retourne les coordonnées du vecteur crée par les deux points A et B en entrée
+* @param {number}  A de coordonnées x et y
+* @param {number} B de coordonnées x et y
+* @returns {Object} outputVector coordonnées du vecteur
+*/
+function getVectorCoords(A,B){
+    let outputVector = {x:undefined,y:undefined}
+    outputVector.x = B.x - A.x;
+    outputVector.y = B.y - A.y;
+    return outputVector;
 }
