210eef699e3f105a98082898eb12dee36939a565
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-04-09 16:22:20 +0200

    WIP:création batterie de test

--- commands/vacheAPIs.js
+++ commands/vacheAPIs.js
@@ -22,3 +22,53 @@ function ajoutPiquet(coordX,coordY){
       }
     enclos.push(nouveauPiquet);
 }
+
+/* Batterie de tests **/
+function lancementTests(){
+    ajoutPiquet(-1,1);
+    ajoutPiquet(-1,-1);
+    ajoutPiquet(1,-1);
+    ajoutPiquet(1,1);
+    let aire = 4;
+    let centreGrav = { "x":0, "y":0};
+    let vacheInterieur = true;
+
+
+    testEnclos(enclos,aire,centreGrav,vacheInterieur);
+
+    enclos =[];
+
+    ajoutPiquet(-16.6, -20.1);
+    ajoutPiquet(-12.6, -18.6);
+    ajoutPiquet(-11.6, -16.6);
+    ajoutPiquet(-15.1, -15.1);
+    aire = 13.125;
+    centreGrav = { "x":-14.226, "y":-17.555};
+    vacheInterieur = true;
+
+    testEnclos(enclos,aire,centreGrav,vacheInterieur);
+    enclos=[]; // remise à zéro de l'enclos
+
+    ajoutPiquet(-1.1, -1.5);
+    ajoutPiquet(2.1, 3.012);
+    ajoutPiquet(5.6, -1.21);
+    ajoutPiquet(1.97, 4.07);
+
+    aire = 3.563;
+    centreGrav = { "x":1.978, "y":1.903};
+    vacheInterieur = false;
+    testEnclos(enclos,aire,centreGrav,vacheInterieur);
+
+}
+
+/**
+ Ajoute un
+* @param {Array} enclosEntree Tableau de points de l'enclos
+* @param {Number} aireAttendue Aire de l'enclos calculée
+* @param {Object} centreGravAttendu centre de gravité obtenu
+* @param {Boolean} presenceAttendue Présence ou non de la vache dans l'enclos
+* @param {Array} enclosEntree 
+*/
+function testEnclos(enclosEntree,aireAttendue,centreGravAttendu,presenceAttendue){
+    
+}
