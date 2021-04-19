8078f4b956bd956907e813a732423817f3b2b8d1
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-04-19 12:07:33 +0200

    commande enclos basique /spent 30m

--- commands/enclos.js
+++ commands/enclos.js
@@ -1,16 +1,27 @@
+const { vacheEnclos } = require("../libs/vacheAPIs");
+
 let debugstring = "this is a debug string";
 
 module.exports = {
     execute(message,args){
-            if(args == "ajout"){
-                message.channel.send("ajout de piquet ");
-            }
-            if(args == "retirer"){
-                message.channel.send("retirer piquet ");
-            }
-            if(args == "raz"){
-                message.channel.send("remise a zero ");
-            }
+        if (!args.length) {
+            return message.channel.send(
+                `Aucun argument saisi !, ${message.author}!`);
+        }
+        else switch (args[0]) {
+            case "a":
+                return message.channel.send("ajouter");
+                break;
+            case "r":
+                return message.channel.send("retirer");
+            case "raz":
+                return message.channel.send("remise a zéro !");
+
+            default:
+                return message.channel.send(
+                    "j'ai mal saisi votre premier argument, faites `!aide`");
+                break;
+        }
     },
     name: "enclos",
 };
