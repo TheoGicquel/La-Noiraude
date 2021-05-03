736b9b81ce790dada9043ac16fb95e4baf0aec7e
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-03 10:17:46 +0200

    [fix] crash commande etat

--- commands/status.js
+++ commands/status.js
@@ -1,8 +1,13 @@
 module.exports = {
     execute(message,args){
+        console.log(global.enclosVache);
+        message.channel.send(JSON.stringify(global.enclosVache));
+        /*
         for(let i=0;i<enclosVache.length;i++){
+            
             message.channel.send(global.enclosVache[i]);
         }
+        */
         
     },
     name: "etat",
