ef1a7fb07bc6814c3856a11aa15e94b7d8725fc3
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-06 11:31:00 +0200

    création commande !info pour afficher les infos sur le proje

---
+++ commands/info.js
@@ -0,0 +1,23 @@
+/**
+ * @file Commande d'affichage des commandes disponibles
+ * @author Theo Gicquel 
+ */
+
+/** Message à afficher */
+const info = `
+    **La noiraude** est un robot discord de calcul de centre de gravité réalisé par:
+    - Théo Gicquel https://gitlab.com/TheoGicquel
+    - Julien Mocquet https://gitlab.com/jujugibus
+
+    Réalisé du 02 Avril 2021 au 15 Mai 2021
+    Le code source de ce projet est consultable sur https://gitlab.com/TheoGicquel/la-noiraude-bot
+
+`;
+
+/** exportation de la commande */
+module.exports = {
+    execute(message,args){
+        message.channel.send(info);
+    },
+    name: "info",
+};
