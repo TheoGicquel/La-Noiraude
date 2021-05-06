b58ea025cd8557ae9bd30d0792a1ce50646f6537
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-06 11:30:33 +0200

    ajout des commandes aux infos affichées par !aide

--- commands/aide.js
+++ commands/aide.js
@@ -5,8 +5,20 @@
 
 /** Message à afficher */
 const aideMessage = `
-\`!aide\`\t affiche cette commande
-\`!Hello\`\t dire bonjour
+(réalisé par Théo Gicquel & Julien mocquet)
+** Commandes de base **
+\`!aide\`\t Affichage de la liste de toutes les commandes
+
+
+\`!etat\`\t afficher les piquets de l'enclos
+\`!enclos\`\t modifier précisément l'enclos 
+\`!saisie\`\t saisir un ensemble de piquets de l'enclos simultanément
+
+\`!lancement\`\t déterminer si la vache se trouve dans le pré
+
+** Informations & Cosmétique **
+\`!info\`\t voir les informations sur ce robot
+\`!hello\`\t dire bonjour
 \`!dance\`\t ???
 `;
 
