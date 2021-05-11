a26cb7d4eae672ff654c58f741ea632c6ea31c20
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-11 11:15:25 +0000

    Fix controle commande saisie char illegaux

--- commands/saisie.js
+++ commands/saisie.js
@@ -9,19 +9,25 @@ let mathAPI = require('../libs/mathAPI.js');
 let noiraude = require('../libs/noiraude');
 let io = require('../libs/io');
 let saisieHelpText="Format :\n `!saisie x,y:x,y etc...`"
-
+//FIXME Il faudra modifier l'indentation.. c'est illisible
 module.exports = {
     execute(message,args){
         switch (args.length) {
             case 1:
-                noiraude.enclos=io.saisieParser(args[0]);
-                return message.channel.send("Saisie effectuee ");
-                break;
+                let valid = io.saisieValidation(args[0]);
+                switch (valid) {
+                    case true:
+                        noiraude.enclos=io.saisieParser(args[0]);
+                        return message.channel.send(":white_check_mark: Saisie effectuée !\n" + io.jsonPrettifier(noiraude.enclos)); 
+                    case false:
+                        return message.channel.send(":x: Saisie invalide !");
+                    default:
+                        return message.channel.send(":thinking: Problème de saisie innatendu");
+                }
             default:
                 message.channel.send(
-                    `Nombre d'arguments incorrects !, ${args.length} au lieu de 2`);
+                    `Nombre d'arguments incorrects !  ${args.length} au lieu de 2`);
                     return message.channel.send(saisieHelpText);
-                break;
         }
     },
     name: "saisie",
