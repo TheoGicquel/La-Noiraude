7a19e55973119b8a741df1541e3b89af47e6a77c
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-12 12:00:13 +0200

    commentaires dance.js

--- commands/dance.js
+++ commands/dance.js
@@ -1,3 +1,14 @@
+/**
+ * @file dance.js
+ * @fileoverview Commande d'affichage d'un gif animé
+ * @author Theo Gicquel 
+ */
+
+/**
+ * Chaîne de caractères de l'url du gif
+ * @type {String}
+ * @const
+ */
 var danceUrl = "https://tenor.com/view/polish-dancing-cow-dancing-cow-polish-gif-18921101";
 module.exports = {
     execute(message,args){
