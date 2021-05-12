b2425b71f2936599a068c60b84d1e5bec5eee775
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-12 14:55:08 +0200

    [doc] !etat

--- commands/status.js
+++ commands/status.js
@@ -1,5 +1,15 @@
+/**
+ * @file etat.js
+ * @usage `!etat`
+ * @fileoverview Commande d'affichage du contenu actuel de l'enclos
+ * @author Theo Gicquel <theo.gicquel.work@gmail.com>
+ */
+
+/** requisistion des modules nécessaires */
 let noiraude = require('../libs/noiraude');
 let io = require('../libs/io')
+
+/** Exportation du module local en commande appellée via son attribut `name`*/
 module.exports = {
     execute(message,args){
         message.channel.send(io.jsonPrettifier(noiraude.enclos));
