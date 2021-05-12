0afb48013b296a36d643547ebc7ddd192a9c81d3
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-12 16:39:50 +0200

    hello JS non conforme camelCase

--- commands/Hello.js
+++ commands/Hello.js
@@ -1,5 +1,5 @@
 /**
- * @file Hello.js
+ * @file hello.js
  * @usage `!hello`
  * @fileoverview Affiche un message de salutation aléatoire
  * @author Julien Mocquet
@@ -10,7 +10,7 @@
  * @type {Array}
  * @const
  */
-const ListHello = [ 
+const listHello = [ 
     'Hello les Loulous',
     'Bonjour les amis',
     "Allo Docteur ici la Noiraude",
@@ -20,7 +20,7 @@ const ListHello = [
 module.exports = {
     execute(message,args){ 
         //FIXME @jujugibus Julien mocquet : longueur ligne !
-        message.channel.send(`${ListHello[Math.floor(Math.random() * ListHello.length)]}`); //floor retire la décimal // Math.random = un chiffre entre 0 et 1 (exclus)
+        message.channel.send(`${listHello[Math.floor(Math.random() * listHello.length)]}`); //floor retire la décimal // Math.random = un chiffre entre 0 et 1 (exclus)
     },
     name: "hello",
 };
