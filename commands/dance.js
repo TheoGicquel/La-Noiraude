bf57ba75ef9483ed29e8fe57f82103aa93979e8a
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-12 14:26:37 +0000

    Cleanup julien

--- commands/dance.js
+++ commands/dance.js
@@ -1,3 +1,11 @@
+/**
+ * @file dance.js
+ * @usage `!dance`
+ * @fileoverview Commande d'affichage de gif
+ * @author Theo Gicquel
+ * @author Julien Mocquet 
+ */
+
 var danceUrl = "https://tenor.com/view/polish-dancing-cow-dancing-cow-polish-gif-18921101";
 module.exports = {
     execute(message,args){
--- index.js
+++ index.js
@@ -3,15 +3,15 @@
  * @requires discord.js
  * @fileoverview Racine du robot discord
  * @author Theo Gicquel <theo.gicquel.work@gmail.com>
- * @author Julien Mocquet <email>
+ * @author Julien Mocquet <julien.mocquet45@gmail.com>
  */
-/** Importation librairies */
+/* Importation librairies */
 const Discord = require('discord.js')
 bot = new Discord.Client();
 var vache = require('./libs/vacheAPIs.js');
-var mathAPI = require('./libs/mathAPI.js');
-var config = require('./config.json');
-var fs = require("fs")
+var mathAPI = require('./libs/mathAPI.js'); //Librairie des calculs pour l'enclos
+var config = require('./config.json'); //Fichier de configuration avec le préfixe et le token de connexion
+var fs = require("fs") //Librairies de gestion de fichiers
 var noiraude = require('./libs/noiraude');
 bot.on("error", (e) => console.error(e));
 bot.on("warn", (e) => console.warn(e));
@@ -45,7 +45,7 @@ bot.on('message', message => {
     }
 
     /** Parsage message saisi
-     *  - separation 
+     *  - separation pour récupérer les arguments (Ex : Postion du piquet)
      */
     const args = message.content.split(/ +/)
     //On récupère la commande et la vérifie
@@ -56,7 +56,7 @@ bot.on('message', message => {
     // Si la commande ne commence pas par le prefixe
     if (!commandName.startsWith(config.prefix)) return 
 
-    //La "commandS" est recherchée en fonction du commandName duquel on a retiré le préfixe
+    //La "commandS" est recherchée en fonction du commandName duquel on a retiré le préfixe dans la collection de commande du bot
     const command = bot.commands.get(commandName.slice(config.prefix.length)) 
     
     // Si c'est une commande qui n'existe pas
