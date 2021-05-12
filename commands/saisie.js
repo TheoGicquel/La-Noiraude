565728d07e487a21b34a7038273812ec229281af
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-05-12 14:53:22 +0200

    [doc] WIP: !saisie

--- commands/saisie.js
+++ commands/saisie.js
@@ -4,18 +4,22 @@
  * @fileoverview Commande de saisie de multiples piquets via une seule commande
  * @author Theo Gicquel <theo.gicquel.work@gmail.com>
  */
+
+/** requisistion des modules nécessaires */
 let vache = require('../libs/vacheAPIs.js');
 let mathAPI = require('../libs/mathAPI.js');
 let noiraude = require('../libs/noiraude');
 let io = require('../libs/io');
 let saisieHelpText="Format :\n `!saisie x,y:x,y etc...`"
-//FIXME Il faudra modifier l'indentation.. c'est illisible
+let validation;
+
+/** Exportation du module local en commande appellée via son attribut `name`*/
 module.exports = {
     execute(message,args){
         switch (args.length) {
             case 1:
-                let valid = io.saisieValidation(args[0]);
-                switch (valid) {
+                validation = io.saisieValidation(args[0]);
+                switch (validation) {
                     case true:
                         noiraude.enclos=io.saisieParser(args[0]);
                         return message.channel.send(":white_check_mark: Saisie effectuée !\n" + io.jsonPrettifier(noiraude.enclos)); 
